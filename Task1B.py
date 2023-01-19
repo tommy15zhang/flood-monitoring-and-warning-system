d65108f1f6a390d03dc361cd10f570f50d03ff1a
tommzhaanghg <49993277+tommzhaanghg@users.noreply.github.com>
2023-01-19 23:38:00 +0000

    .

--- Task1B.py
+++ Task1B.py
@@ -1,9 +1,12 @@
 from floodsystem.geo import station_by_distance
 from floodsystem.stationdata import build_station_list
 
+
+
+
+
 p = (52.2053, 0.1218) #Coordiante of Cambridge City Centre
 x = station_by_distance(build_station_list(), p)
-
 print(f"the closest 10 entries are {x[:10]}")
 print(f"the furthest 10 entries are {x[-10:]}")
 
