c89d57c1fe9b1048f32d8a74aaba675718ba6eee
IdoBalter <123095083+IdoBalter@users.noreply.github.com>
2023-01-30 22:38:11 +0000

    Task D update from Ido

--- Task1D.py
+++ Task1D.py
@@ -9,4 +9,5 @@ rivers = rivers_with_station(stations)
 station_river = stations_by_river(stations)
 print(f"{len(rivers)} rivers have at least one station. First 10 - {rivers[0:9]}")
 print(f"River Aire {sorted(station_river['River Aire'])}")
+print(f"River Cam {sorted(station_river['River Cam'])}"
 print(f"River Thames{sorted(station_river['River Thames'])}")
